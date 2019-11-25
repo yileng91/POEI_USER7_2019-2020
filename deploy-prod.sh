174fec307e2d5f5f37ddac940169dcea92534660
vang-neva <vangneva.ku@gmail.com>
2019-11-25 10:20:20 +0100

    Ajout fex-permission.sh

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#mysqldump -h host -u user -ppass -rfichier base_de_donnees [tables]
+
 # récupérer le code source
 git pull origin master
 
@@ -14,9 +16,15 @@ drush updb -y
 
 # exporter la config prod
 drush csex prod -y
+git add config/prod
+git commit -m 'Ajout des configs de prod'
+git push origin master
 
 # import les configs de dév
 drush cim -y
 
 # vider le cache
 drush cr
+
+#sudo bash fix-permissions.sh --drupal_path=your/drupal/path --drupal_user=your_user_name
+bash fix-permissions.sh --drupal_path=web --drupal_user=www-data
